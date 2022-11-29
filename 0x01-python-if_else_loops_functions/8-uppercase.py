fa65a8a9a999f0f62ee4812eb8bfabc0269b5831
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-11-29 11:50:15 -0800

    Upper case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for c in str:
         if ord(c) > 96 and ord(c) < 123:
-            letter = ord(ord(c) - 32)
+            letter = chr(ord(c) - 32)
         else:
             letter = c
         print("{}".format(letter), end="")
