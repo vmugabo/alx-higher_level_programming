dbc99b33fc837cfff9865c1c7cd057a1e3b0e3f2
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-11-29 12:25:47 -0800

    last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_num = number % 10
-    return (last_num)
+    if number < 0:
+        number = -number
+    last = number % 10
+    print(last, end='')
+    return last
