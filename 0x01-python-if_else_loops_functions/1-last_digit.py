f81446dea288838e5c6b14e85d869046440ed1f8
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-11-29 03:03:01 -0800

    Complied with pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ num_str = str(number)
 last_num = num_str[-1]
 if int(last_num) > 5:
     str = ("and is greater than 5")
-elif (int(last_num) < 6) and (int(last_num)!= 0):
+elif (int(last_num) < 6) and (int(last_num) != 0):
     str = ("and is less than 6 and not 0")
 else:
     str = ("and is 0")
