d0aae29db52bbf88e674810bae343c79ad1bc321
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-12-05 03:28:52 -0800

    replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if type(my_list) == list:
         cp_list = my_list.copy()
-    if idx < 0 or idx > len(cp_list):
+    if idx < 0 or idx > len(cp_list)-1:
         return cp_list
     else:
         cp_list[idx] = element
