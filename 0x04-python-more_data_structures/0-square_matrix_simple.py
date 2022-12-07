db968dd2bc49f826f1f9ac8ef98773d3d343910a
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-12-07 09:16:23 -0800

    Squared matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_values(matrix):
-    squared_matrix = map(lambda x: x**2, matrix)
+    squared_matrix = map(lambda x: x**2,row) for row in  matrix
     return list(squared_matrix)
