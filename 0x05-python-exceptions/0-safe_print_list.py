fcb941f1e70a9123f2997755160e8fde510fb2c3
Vincent Mugabo <vicentmugabo75@gmail.com>
2022-12-19 07:52:24 -0800

    correct shebang

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
     sum = 0
     for i in range(x):
         try:
             print(f"{my_list[i]}", end="")
-            sum +=1
+            sum += 1
         except IndexError:
             break
     print()
-    return(sum)
+
+    return (sum)
