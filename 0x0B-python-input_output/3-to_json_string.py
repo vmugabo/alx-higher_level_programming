092031d1b212a275220478811d4c8509e2e63031
Vincent Mugabo <vicentmugabo75@gmail.com>
2023-01-10 11:49:06 -0800

    JSON

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module 3-to_json_string.
+Returns the JSON representation of an object.
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns th JSON representation of my_obj.
+
+    Args:
+        - my_obj: string to represent
+
+    Returns: JSON representation
+    """
+
+    return json.dumps(my_obj)
+(base) vinny@Vincents-MacBook-Pro 0x0B-python-input_output % cat 5-to_json_string.py 
+#!/usr/bin/python3
+"""Module 5-to_json_string.
+Returns the JSON representation of an object.
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns th JSON representation of my_obj.
+
+    Args:
+        - my_obj: string to represent
+
+    Returns: JSON representation
+    """
+
+    return json.dumps(my_obj)
