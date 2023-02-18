77ed4a7468e9167916f5fde78a1340459a96b533
Vincent Mugabo <vicentmugabo75@gmail.com>
2023-02-19 00:47:55 +0200

    Changed Rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,120 +1,131 @@
 #!/usr/bin/python3
 
-from models.base import Base
-"""_summary_
-
-    Raises:
-        ValueError: _description_
-        ValueError: _description_
-        ValueError: _description_
-        ValueError: _description_
-
-    Returns:
-        _type_: _description_
-    """
 class Rectangle(Base):
-    """_summary_
-
-    Args:
-        Base (_type_): _description_
     """
+    A class that represents a rectangle.
+
+    Attributes:
+        id (int): The unique identifier for the rectangle.
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int): The x-coordinate of the rectangle's top-left corner.
+        y (int): The y-coordinate of the rectangle's top-left corner.
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        """_summary_
+        """
+        Initializes a new Rectangle object with the specified width, height, x-coordinate,
+        y-coordinate, and optional identifier.
 
         Args:
-            width (_type_): _description_
-            height (_type_): _description_
-            x (int, optional): _description_. Defaults to 0.
-            y (int, optional): _description_. Defaults to 0.
-            id (_type_, optional): _description_. Defaults to None.
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle's top-left corner. Defaults to 0.
+            y (int, optional): The y-coordinate of the rectangle's top-left corner. Defaults to 0.
+            id (int, optional): The unique identifier for the rectangle. Defaults to None.
         """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
-   
+
     @property
     def width(self):
-        """_summary_
+        """
+        Returns the width of the rectangle.
 
         Returns:
-            _type_: _description_
+            int: The width of the rectangle.
         """
         return self.__width
-    @property
-    def height(self):
-        """_summary_
 
-        Returns:
-            _type_: _description_
-        """
-        return self.__height
-    @property
-    def x(self):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
-        return self.__x
-    @property
-    def y(self):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
-        return self.__y
     @width.setter
     def width(self, value):
-        """_summary_
+        """
+        Sets the width of the rectangle.
 
         Args:
-            value (_type_): _description_
+            value (int): The new width of the rectangle.
 
         Raises:
-            ValueError: _description_
+            ValueError: If the new width is less than or equal to 0.
         """
         if value <= 0:
             raise ValueError("Width must be positive")
-        self.__width == value
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Returns the height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
+
     @height.setter
     def height(self, value):
-        """_summary_
+        """
+        Sets the height of the rectangle.
 
         Args:
-            value (_type_): _description_
+            value (int): The new height of the rectangle.
 
         Raises:
-            ValueError: _description_
+            ValueError: If the new height is less than or equal to 0.
         """
         if value <= 0:
-            raise ValueError("height must be positive")
-        self.__height == value
+            raise ValueError("Height must be positive")
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Returns the x-coordinate of the rectangle's top-left corner.
+
+        Returns:
+            int: The x-coordinate of the rectangle's top-left corner.
+        """
+        return self.__x
+
     @x.setter
     def x(self, value):
-        """_summary_
+        """
+        Sets the x-coordinate of the rectangle's top-left corner.
 
         Args:
-            value (_type_): _description_
+            value (int): The new x-coordinate of the rectangle's top-left corner.
 
         Raises:
-            ValueError: _description_
+            ValueError: If the new x-coordinate is less than 0.
         """
-        if value <= 0:
-            raise ValueError("x must be positive")
-        self.__x == value
+        if value < 0:
+            raise ValueError("X must be non-negative")
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Returns the y-coordinate of the rectangle's top-left corner.
+
+        Returns:
+            int: The y-coordinate of the rectangle's top-left corner.
+        """
+        return self.__y
+
     @y.setter
     def y(self, value):
-        """_summary_
+        """
+        Sets the y-coordinate of the rectangle's top-left corner.
 
         Args:
-            value (_type_): _description_
+            value (int): The new y-coordinate of the rectangle's top-left corner.
 
         Raises:
-            ValueError: _description_
+            ValueError: If the new y-coordinate is less than 0.
         """
-        if value <= 0:
-            raise ValueError("y must be positive")
-        self.__y == value    
+        if value < 0:
+            raise ValueError("Y must be non-negative")
+        self.__y = value
