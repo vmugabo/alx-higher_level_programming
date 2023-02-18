e28a4b242f1158704a2120dfd1c79545b435ff5f
Vincent Mugabo <vicentmugabo75@gmail.com>
2023-02-19 00:38:42 +0200

    Pycodestyle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,9 +2,11 @@
 
 """Define class with private __nb_objects """
 class Base:
+
+
     """Base Class with __nb_objects as private class attribute"""
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """Initializes Base Instance"""
         if id is not None:
@@ -12,6 +14,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
-            
-                
