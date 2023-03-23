a29b5076de139a9cd9f9207eb192f97e42d47a6c
Vincent Mugabo <vicentmugabo75@gmail.com>
2023-03-23 09:51:37 -0700

    SQL

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,3 @@
--- creates the MySQL server user user_0d_1
--- creates a new user
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
--- grants privileges
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
-(base) vinny@Vincents-MacBook-Pro 0x0E-SQL_more_queries % cat 2-create_read_user.sql
 -- creates the database hbtn_0d_2 and the user user_0d_2
 -- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
